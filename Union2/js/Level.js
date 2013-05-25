138373916641718e334492697187b36e2a517133
Roy Dorombozi <roy.dorombozi@hotmail.com>
2013-05-25 15:03:48 -0700

    Forgot to add a file, richard parker is upset and will spray feces to my face

---
+++ Union2/js/Level.js
@@ -0,0 +1,18 @@
+﻿/**
+ * Level
+ * A class which represents a level.
+ */
+var Level = function () {
+    this.GameBoard = [];
+    this.SelectedTiles = [];
+};
+
+/**
+ * A function which is the callback when we click on the passed in dom element.
+ */
+Level.prototype.OnClick = function (domElement) {
+    
+    // This makes an array of selected tiles, but if you clicked the same one twice it doesn't.
+    this.SelectedTiles[domElement] = domElement;
+    this.GameBoard[domElement];
+};

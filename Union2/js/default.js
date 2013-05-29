668a14065ad71fcd624c272eaa415b50c2053989
Nam Ngo <davidminhnamngo@gmail.com>
2013-05-28 21:58:14 -0700

    porno music

--- Union2/js/default.js
+++ Union2/js/default.js
@@ -18,6 +18,7 @@
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
                 // TODO: This application has been newly launched. Initialize
                 // your application here.
+                $('#audioMusic')[0].play();
             } else {
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
